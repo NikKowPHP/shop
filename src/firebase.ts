c141041d66f480ad44fde103c131ac728ae84de8
NikKowPHP <nik.kow@outlook.com>
2024-03-05 15:55:07 +0100

    export firebase db

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB0nIGFlnqAMZw61nsKUv5zjK9jiFpvDn4",
@@ -7,10 +8,11 @@ const firebaseConfig = {
   projectId: "shop-development-d1527",
   storageBucket: "shop-development-d1527.appspot.com",
   messagingSenderId: "367198258793",
-  appId: "1:367198258793:web:50b0f76ae6f1f464cac4f8"
+  appId: "1:367198258793:web:50b0f76ae6f1f464cac4f8",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export default app;
